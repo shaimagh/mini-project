952790324327d33ba53ccdbb30bbda1e250e6520
ahmedbensai <99761942+ahmedbensai@users.noreply.github.com>
2022-04-11 00:07:46 +0100

    Add files via upload

---
+++ src/app/paiment/paiment.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaimentComponent } from './paiment.component';
+
+describe('PaimentComponent', () => {
+  let component: PaimentComponent;
+  let fixture: ComponentFixture<PaimentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PaimentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PaimentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/paiment/paiment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paiment',
+  templateUrl: './paiment.component.html',
+  styleUrls: ['./paiment.component.css']
+})
+export class PaimentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
